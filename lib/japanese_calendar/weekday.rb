708673338682d5e8b037e0b792333e86a7bc168c
Ryo Yamamoto <email@ryoyamamoto.jp>
2019-04-04 09:57:13 +0900

    Extract deprecation pattern and messages to constants

--- lib/japanese_calendar/weekday.rb
+++ lib/japanese_calendar/weekday.rb
@@ -13,13 +13,23 @@ module JapaneseCalendar
     #
     #   date_of_birth.strftime("%-Y年%-m月%-d日(%Ja)") # => "1978年7月19日(水)"
     def strftime(format)
-      deprecate('%Q', 'Please use %JA instead.') if format =~ /%Q/
-      deprecate('%q', 'Please use %Ja instead.') if format =~ /%q/
+      deprecated_directives = format.scan(DEPRECATION_PATTERN).uniq
+      deprecated_directives.each do |key|
+        deprecate(key, DEPRECATION_MESSAGES[key])
+      end
+
       string = format.gsub(weekday_pattern, weekday_conversion)
       super(string)
     end
 
     private
+
+      DEPRECATION_MESSAGES = {
+        '%Q' => 'Please use %JA instead.',
+        '%q' => 'Please use %Ja instead.'
+      }.freeze
+
+      DEPRECATION_PATTERN = Regexp.union(DEPRECATION_MESSAGES.keys)
 
       def weekday_name
         @weekday_name ||= %w(日曜日 月曜日 火曜日 水曜日 木曜日 金曜日 土曜日)[wday]
