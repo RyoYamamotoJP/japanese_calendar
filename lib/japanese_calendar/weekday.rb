5dbc21adacf96eba93fbe018bad2176682cb50d3
Ryo Yamamoto <email@ryoyamamoto.jp>
2019-04-04 13:31:47 +0900

    Use `[` and `]` instead of `(` and `)`

--- lib/japanese_calendar/weekday.rb
+++ lib/japanese_calendar/weekday.rb
@@ -36,7 +36,7 @@ module JapaneseCalendar
       end
 
       def weekday_name
-        @weekday_name ||= %w(日曜日 月曜日 火曜日 水曜日 木曜日 金曜日 土曜日)[wday]
+        @weekday_name ||= %w[日曜日 月曜日 火曜日 水曜日 木曜日 金曜日 土曜日][wday]
       end
 
       def weekday_abbreviation
