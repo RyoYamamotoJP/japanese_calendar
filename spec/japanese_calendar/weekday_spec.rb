41f9f3abadafb2eda47bb50dc106d9d580d2ff32
Ryo Yamamoto <email@ryoyamamoto.jp>
2019-04-10 13:49:13 +0900

    Refactor weekday specs

--- spec/japanese_calendar/weekday_spec.rb
+++ spec/japanese_calendar/weekday_spec.rb
@@ -3,80 +3,86 @@
 require 'spec_helper'
 
 describe JapaneseCalendar::Weekday do
-  shared_examples 'a weekday' do |weekday|
+  shared_examples 'weekday examples' do |full_name, abbreviated_name|
     describe '#strftime' do
-      context 'with %JA format' do
-        let(:format) { '%JA' }
-
-        it "returns \"#{weekday}\"" do
-          expect(subject.strftime(format)).to eq(weekday)
+      context 'with "%JA" format' do
+        it "returns \"#{full_name}\"" do
+          expect(subject.strftime('%JA')).to eq(full_name)
         end
       end
 
-      context 'with %Ja format' do
-        let(:format) { '%Ja' }
-
-        it "returns \"#{weekday[0]}\"" do
-          expect(subject.strftime(format)).to eq(weekday[0])
+      context 'with "%Ja" format' do
+        it "returns \"#{abbreviated_name}\"" do
+          expect(subject.strftime('%Ja')).to eq(abbreviated_name)
         end
       end
 
-      context 'with %Q format' do
-        let(:format) { '%Q' }
-
-        it "returns \"#{weekday}\" with a warning" do
-          expect(subject.strftime(format)).to eq(weekday)
+      context 'with "%Q" format', if: (described_class == Time) do
+        it "returns \"#{full_name}\" with a warning" do
+          expect(subject.strftime('%Q')).to eq(full_name)
           expect do
-            subject.strftime(format)
+            subject.strftime('%Q')
           end.to output(/%Q is deprecated. Please use %JA instead./).to_stderr
         end
       end
 
-      context 'with %q format' do
-        let(:format) { '%q' }
-
-        it "returns \"#{weekday[0]}\" with a warning" do
-          expect(subject.strftime(format)).to eq(weekday[0])
+      context 'with "%q" format' do
+        it "returns \"#{abbreviated_name}\" with a warning" do
+          expect(subject.strftime('%q')).to eq(abbreviated_name)
           expect do
-            subject.strftime(format)
+            subject.strftime('%q')
           end.to output(/%q is deprecated. Please use %Ja instead./).to_stderr
         end
       end
     end
   end
 
-  context 'on Sunday' do
-    subject { Time.new(2016, 12, 11) }
-    it_behaves_like 'a weekday', '日曜日'
+  shared_examples 'a weekday' do
+    context 'on Sunday' do
+      subject { described_class.new(2016, 12, 11) }
+      include_examples 'weekday examples', '日曜日', '日'
+    end
+
+    context 'on Monday' do
+      subject { described_class.new(2016, 12, 12) }
+      include_examples 'weekday examples', '月曜日', '月'
+    end
+
+    context 'on Tuesday' do
+      subject { described_class.new(2016, 12, 13) }
+      include_examples 'weekday examples', '火曜日', '火'
+    end
+
+    context 'on Wednesday' do
+      subject { described_class.new(2016, 12, 14) }
+      include_examples 'weekday examples', '水曜日', '水'
+    end
+
+    context 'on Thursday' do
+      subject { described_class.new(2016, 12, 15) }
+      include_examples 'weekday examples', '木曜日', '木'
+    end
+
+    context 'on Friday' do
+      subject { described_class.new(2016, 12, 16) }
+      include_examples 'weekday examples', '金曜日', '金'
+    end
+
+    context 'on Saturday' do
+      subject { described_class.new(2016, 12, 17) }
+      include_examples 'weekday examples', '土曜日', '土'
+    end
   end
 
-  context 'on Monday' do
-    subject { Time.new(2016, 12, 12) }
-    it_behaves_like 'a weekday', '月曜日'
+  describe Date do
+    it_behaves_like 'a weekday'
   end
 
-  context 'on Tuesday' do
-    subject { Time.new(2016, 12, 13) }
-    it_behaves_like 'a weekday', '火曜日'
+  describe DateTime do
+    it_behaves_like 'a weekday'
   end
 
-  context 'on Wednesday' do
-    subject { Time.new(2016, 12, 14) }
-    it_behaves_like 'a weekday', '水曜日'
-  end
-
-  context 'on Thursday' do
-    subject { Time.new(2016, 12, 15) }
-    it_behaves_like 'a weekday', '木曜日'
-  end
-
-  context 'on Friday' do
-    subject { Time.new(2016, 12, 16) }
-    it_behaves_like 'a weekday', '金曜日'
-  end
-
-  context 'on Saturday' do
-    subject { Time.new(2016, 12, 17) }
-    it_behaves_like 'a weekday', '土曜日'
+  describe Time do
+    it_behaves_like 'a weekday'
   end
 end
